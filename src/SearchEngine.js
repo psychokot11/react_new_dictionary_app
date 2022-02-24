efc1808701a6025e5bd7b426ad7985d0c8af6433
psychokot11 <z.stawarska@gmail.com>
2022-02-25 00:27:43 +0100

    Used axios to call API

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 import Word from "./Word";
 import Images from "./Images";
 
 export default function SearchEngine() {
   const [searchedWord, setSearchedWord] = useState("moon");
+  const [wordData, setWordData] = useState(null);
+  const [photoData, setPhotoData] = useState(null);
+
+  function handleDictionaryResponse(response) {
+    setWordData(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotoData(response.data.photos);
+    console.log(response.data.photos);
+  }
+
+  function search() {
+    let dictionaryURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`;
+    axios.get(dictionaryURL).then(handleDictionaryResponse);
+    let pexelsKey = "563492ad6f91700001000001daedf3e1b4b646f88958b096669822ab";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${searchedWord}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsKey}` };
+    axios.get(pexelsUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handleSearch(event) {
+    setSearchedWord(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    search();
   }
-  function handleSearch(event) {}
 
   return (
     <div className="SearchEngine">
