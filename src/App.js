78400e943c9f7df47596d3b567112b92be7b1f5c
psychokot11 <z.stawarska@gmail.com>
2022-02-24 23:25:28 +0100

    Instaled some npm's
    Axios
    npm i axios
    Bootstrap
    npm i bootstrap
    import "bootstrap/dist/css/bootstrap.css";
    import "bootstrap";

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container">
+      Hello reactkkk
+      <button className="btn btn-primary"></button>
+    </div>
+  );
 }
 
 export default App;
